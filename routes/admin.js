f2e510e90a3e993dc7bd2cbe353d0bd8d48859b0
Justin Joseph <mailtojustinabin@gmail.com>
2021-01-19 20:20:27 +0530

    mvc Added

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,16 @@
 const path = require('path');
+
 const express = require('express');
+const productsController = require('../controller/products')
 
 const rootDir = require('../util/path');
+
 const router = express.Router();
 
-//Get Requesr
-router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','add-product.html'))
-});
+// /admin/add-product => GET
+router.get('/add-product',productsController.getAddProduct)
 
-// Post Request
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const path = require('path');
+
 const express = require('express');
-const rootDir = require('../util/path');
+const productController = require('../controller/products')
+
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir ,'views','shop.html'))
-});
+router.get('/',productController.getProducts);
+
 module.exports = router;
